899cca601971cd3476b0b682f4e6cae39cadf9bc
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-04-24 09:42:24 +0900

    feat: init

---
+++ api.php
@@ -0,0 +1,416 @@
+<?php
+ini_set('display_errors', 0);
+
+require_once(__DIR__ . "/config.inc.php");
+
+function initTable()
+{
+    global $pdo;
+    // ユーザーテーブル
+    // id: ユーザーID
+    // ip: IPアドレス
+    $pdo->exec("CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ip VARCHAR(255) NOT NULL
+    )");
+
+    // 投票テーブル
+    // id: 投票ユニークID
+    // user_id: ユーザーID
+    // issue_number: Issue Number
+    // type: 投票タイプ (UP: u, DOWN: d)
+    $pdo->exec("CREATE TABLE IF NOT EXISTS votes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        repo VARCHAR(255) NOT NULL,
+        user_id INTEGER NOT NULL,
+        issue_number INTEGER NOT NULL,
+        type VARCHAR(1) NOT NULL
+    )");
+}
+
+function getUserId()
+{
+    global $pdo;
+    if (!isset($pdo)) {
+        throw new ServerException("データベースの接続に失敗しました。");
+    }
+
+    if (isset($_SESSION["user_id"])) {
+        $stmt = $pdo->prepare("SELECT id FROM users WHERE id = :id");
+        $stmt->bindValue(":id", $_SESSION["user_id"]);
+        $stmt->execute();
+    } else {
+        $stmt = $pdo->prepare("SELECT id FROM users WHERE ip = :ip");
+        $stmt->bindValue(":ip", $_SERVER["REMOTE_ADDR"]);
+        $stmt->execute();
+    }
+    $user = $stmt->fetch();
+    if (!$user) {
+        $stmt = $pdo->prepare("INSERT INTO users (ip) VALUES (:ip)");
+        $stmt->bindValue(":ip", $_SERVER["REMOTE_ADDR"]);
+        $stmt->execute();
+        return $pdo->lastInsertId();
+    }
+    return $user["id"];
+}
+
+function fetchPublicRepos()
+{
+    global $config;
+    $path = $config["file"]["repos"];
+    $token = isset($config["githubToken"]) ? $config["githubToken"] : null;
+    if (file_exists($path)) {
+        $previous = json_decode(file_get_contents($path), true);
+
+        if ($previous["timestamp"] + 60 * 5 <= time()) {
+            return $previous["data"];
+        }
+    }
+    // 5分以上経過している場合は再取得
+    $headers = [
+        "User-Agent: jaoafa/priority-issue-board",
+        "Accept: application/vnd.github.v3+json"
+    ];
+    if ($token) {
+        $headers[] = "Authorization: token $token";
+    }
+
+    $response = file_get_contents("https://api.github.com/orgs/jaoafa/repos?sort=updated&per_page=100", false, stream_context_create([
+        "http" => [
+            "header" => implode("\r\n", $headers),
+            "ignore_errors" => true
+        ]
+    ]));
+    preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
+    $status_code = $matches[1];
+
+    if ($status_code != 200) {
+        if (isset($previous)) {
+            return $previous["data"];
+        }
+        throw new ServerException("リポジトリ一覧の取得に失敗しました。");
+    }
+    $data = json_decode($response, true);
+    $data = array_filter($data, function ($repo) {
+        return !$repo["private"];
+    });
+    file_put_contents($path, json_encode([
+            "timestamp" => time(),
+            "data" => $data
+        ]));
+    return $data;
+}
+
+function fetchIssues($repo)
+{
+    global $config;
+
+    if (file_exists($config["file"]["repos"])) {
+        $repos = json_decode(file_get_contents($config["file"]["repos"]), true);
+        $repos = array_map(function ($repo) {
+            return $repo["name"];
+        }, $repos["data"]);
+        if (!in_array($repo, $repos)) {
+            throw new UserException("対象リポジトリがパブリックではない可能性があるため、Issue を取得できません。");
+        }
+    }
+
+    $path = $config["file"]["issues"];
+    $token = isset($config["githubToken"]) ? $config["githubToken"] : null;
+    $previous = [];
+    if (file_exists($path)) {
+        $previous = json_decode(file_get_contents($path), true);
+
+        if (isset($previous[$repo]) && $previous[$repo]["timestamp"] + 60 * 5 <= time()) {
+            return $previous[$repo]["data"];
+        }
+    }
+    // 5分以上経過している場合は再取得
+    $headers = [
+        "User-Agent: jaoafa/priority-issue-board",
+        "Accept: application/vnd.github.v3+json"
+    ];
+    if ($token) {
+        $headers[] = "Authorization: token $token";
+    }
+
+    $response = file_get_contents("https://api.github.com/repos/jaoafa/$repo/issues?state=open&per_page=100", false, stream_context_create([
+        "http" => [
+            "header" => implode("\r\n", $headers),
+            "ignore_errors" => true
+        ]
+    ]));
+    preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
+    $status_code = $matches[1];
+
+    if ($status_code != 200) {
+        if (isset($previous)) {
+            return $previous["data"];
+        }
+        throw new ServerException("Issue の取得に失敗しました。");
+    }
+    $data = json_decode($response, true);
+    $data = array_filter($data, function ($issue) {
+        return !isset($issue["pull_request"]);
+    });
+    $output = array_merge($previous, [
+        $repo => [
+            "timestamp" => time(),
+            "data" => $data
+        ]
+    ]);
+    file_put_contents($path, json_encode($output));
+    return $data;
+}
+
+function getVoted()
+{
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT * FROM votes WHERE repo = :repo");
+    $stmt->bindValue(":repo", $_GET["repo"]);
+    $stmt->execute();
+
+    $issueUpVotes = []; // issueNumber : [upVoteUserIds]
+    $issueDownVotes = []; // issueNumber : [downVoteUserIds]
+    while ($row = $stmt->fetch()) {
+        $results[$row["issue_number"]] = $row["type"];
+
+        $issueUpVotes[$row["issue_number"]] = isset($issueUpVotes[$row["issue_number"]]) ? $issueUpVotes[$row["issue_number"]] : [];
+        $issueDownVotes[$row["issue_number"]] = isset($issueDownVotes[$row["issue_number"]]) ? $issueDownVotes[$row["issue_number"]] : [];
+
+        if ($row["type"] === "u") {
+            $issueUpVotes[$row["issue_number"]][] = $row["user_id"];
+        } elseif ($row["type"] === "d") {
+            $issueDownVotes[$row["issue_number"]][] = $row["user_id"];
+        }
+    }
+    return [
+        "issueUpVotes" => $issueUpVotes,
+        "issueDownVotes" => $issueDownVotes
+    ];
+}
+
+function vote($repo, $issue_number, $type)
+{
+    global $pdo;
+    $user_id = getUserId();
+    $stmt = $pdo->prepare("SELECT * FROM votes WHERE repo = :repo AND user_id = :user_id AND issue_number = :issue_number");
+    $stmt->bindValue(":repo", $repo);
+    $stmt->bindValue(":user_id", $user_id);
+    $stmt->bindValue(":issue_number", $issue_number);
+    $stmt->execute();
+    $vote = $stmt->fetch();
+    if ($type === null) {
+        if (!$vote) {
+            return;
+        }
+        $stmt = $pdo->prepare("DELETE FROM votes WHERE repo = :repo AND issue_number = :issue_number AND user_id = :user_id");
+        $stmt->bindValue(":repo", $repo);
+        $stmt->bindValue(":issue_number", $issue_number);
+        $stmt->bindValue(":user_id", $user_id);
+        $stmt->execute();
+        return;
+    }
+    if ($vote) {
+        $stmt = $pdo->prepare("UPDATE votes SET type = :type WHERE repo = :repo AND issue_number = :issue_number AND user_id = :user_id");
+        $stmt->bindValue(":repo", $repo);
+        $stmt->bindValue(":issue_number", $issue_number);
+        $stmt->bindValue(":user_id", $user_id);
+        $stmt->bindValue(":type", $type);
+        $stmt->execute();
+    } else {
+        $stmt = $pdo->prepare("INSERT INTO votes (user_id, repo, issue_number, type) VALUES (:user_id, :repo, :issue_number, :type)");
+        $stmt->bindValue(":repo", $repo);
+        $stmt->bindValue(":user_id", $user_id);
+        $stmt->bindValue(":issue_number", $issue_number);
+        $stmt->bindValue(":type", $type);
+        $stmt->execute();
+    }
+}
+
+// 各アクションのサービス関数
+
+function getRepos()
+{
+    $repos = fetchPublicRepos();
+    usort($repos, function ($a, $b) {
+        return ($a["open_issues"] < $b["open_issues"]) ? 1 : -1;
+    });
+    return array_map(function ($repo) {
+        return $repo["name"];
+    }, $repos);
+}
+
+function getIssues()
+{
+    $repo = $_GET["repo"];
+    $userId = getUserId();
+    $issues = fetchIssues($repo);
+    $votes = getVoted();
+
+    $results = [];
+    foreach ($issues as $issue) {
+        $issueUpVotes = isset($votes["issueUpVotes"][$issue["number"]]) ? $votes["issueUpVotes"][$issue["number"]] : [];
+        $issueDownVotes = isset($votes["issueDownVotes"][$issue["number"]]) ? $votes["issueDownVotes"][$issue["number"]] : [];
+        $results[] = [
+            "number" => $issue["number"],
+            "title" => $issue["title"],
+            "author" => [
+                "id" => $issue["user"]["id"],
+                "name" => $issue["user"]["login"],
+            ],
+            "upvote" => [
+                "voted" => in_array($userId, $issueUpVotes),
+                "count" => count($issueUpVotes)
+            ],
+            "downvote" => [
+                "voted" => in_array($userId, $issueDownVotes),
+                "count" => count($issueDownVotes)
+            ],
+            "rank" => count($issueUpVotes) - count($issueDownVotes)
+        ];
+    }
+    return $results;
+}
+
+function upVote()
+{
+    $repo = $_GET["repo"];
+    $number = $_POST["number"];
+    vote($repo, $number, "u");
+}
+
+function downVote()
+{
+    $repo = $_GET["repo"];
+    $number = $_POST["number"];
+    vote($repo, $number, "d");
+}
+
+function resetVote()
+{
+    $repo = $_GET["repo"];
+    $number = $_POST["number"];
+    vote($repo, $number, null);
+}
+
+function isIssueNumber($str)
+{
+    if (!is_numeric($str)) {
+        return false;
+    }
+    return $str > 0;
+}
+
+function checkInput()
+{
+    // GET action は必須
+    if (!isset($_GET["action"])) {
+        throw new UserException("No action specified");
+    }
+    // GET repo は必須
+    if (!isset($_GET["repo"])) {
+        throw new UserException("No repo specified");
+    }
+    // GET repo には / が含まれてはいけない
+    if (mb_strpos($_GET["repo"], "/") !== false) {
+        throw new UserException("Invalid repo");
+    }
+    // POST number が数字でなければならない
+    if (isset($_POST["number"])) {
+        if (!isIssueNumber($_POST["number"])) {
+            throw new UserException("Invalid issue number");
+        }
+    }
+}
+
+function main()
+{
+    // テーブル初期化
+    initTable();
+
+    // 入力確認
+    checkInput();
+
+    // GET ?action=get-issues : Issue の取得
+    if ($_SERVER["REQUEST_METHOD"] === "GET") {
+        if ($_GET["action"] === "get-repos") {
+            return getRepos();
+        }
+        if ($_GET["action"] === "get-issues") {
+            return getIssues();
+        }
+    }
+    // POST ?action=up : +1 投票
+    // POST ?action=down : -1 投票
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if ($_GET["action"] === "up") {
+            return upVote();
+        }
+        if ($_GET["action"] === "down") {
+            return downVote();
+        }
+        if ($_GET["action"] === "reset") {
+            return resetVote();
+        }
+    }
+    throw new UserException("Invalid action");
+}
+
+class ServerException extends Exception
+{
+    public function __construct($message)
+    {
+        parent::__construct($message);
+    }
+}
+
+class UserException extends Exception
+{
+    public function __construct($message)
+    {
+        parent::__construct($message);
+    }
+}
+
+try {
+    header('Content-Type: application/json');
+
+    $result = main();
+    exit(json_encode([
+        "status" => true,
+        "data" => $result
+    ]));
+} catch (ServerException $e) {
+    http_response_code(500);
+    exit(json_encode([
+        "status" => false,
+        "type" => "ServerException",
+        "trace" => explode("\n", $e->getTraceAsString()),
+        "data" => $e->getMessage()
+    ]));
+} catch (UserException $e) {
+    http_response_code(400);
+    exit(json_encode([
+        "status" => false,
+        "type" => "UserException",
+        "trace" => explode("\n", $e->getTraceAsString()),
+        "data" => $e->getMessage()
+    ]));
+} catch (PDOException $e) {
+    http_response_code(500);
+    exit(json_encode([
+        "status" => false,
+        "type" => "PDOException",
+        "trace" => explode("\n", $e->getTraceAsString()),
+        "data" => $e->getMessage()
+    ]));
+} catch (Exception $e) {
+    http_response_code(500);
+    exit(json_encode([
+        "status" => false,
+        "type" => "Exception",
+        "trace" => explode("\n", $e->getTraceAsString()),
+        "data" => $e->getMessage()
+    ]));
+}
---
+++ script.js
@@ -0,0 +1,222 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+        message: "hoge",
+        loading: false,
+        repo: "MyMaid4",
+        repos: [],
+        headers: [
+            {
+                text: '#',
+                align: 'left',
+                sortable: false,
+                value: 'number'
+            },
+            {
+                text: 'タイトル',
+                align: 'left',
+                sortable: false,
+                value: 'title'
+            },
+            {
+                text: '投稿者',
+                align: 'left',
+                sortable: false,
+                value: 'author'
+            },
+            {
+                text: '+1',
+                align: 'left',
+                sortable: true,
+                value: 'upvote',
+                sort: (a, b) => a.count - b.count
+            },
+            {
+                text: '-1',
+                align: 'left',
+                sortable: true,
+                value: 'downvote',
+                sort: (a, b) => a.count - b.count
+            },
+            {
+                text: '見る',
+                align: 'left',
+                sortable: false,
+                value: 'see'
+            },
+        ],
+        items: [],
+        dialog: false,
+    },
+    mounted() {
+        this.getRepos()
+
+        const url = new URL(window.location.href);
+        const params = url.searchParams;
+        if (params.has("repo")) {
+            this.repo = params.get('repo')
+        }
+
+        if (this.repo !== null) {
+            this.getIssues()
+        }
+    },
+    methods: {
+        getRepos() {
+            this.loading = true
+            axios.get(`api.php`, {
+                params: {
+                    repo: this.repo,
+                    action: "get-repos"
+                }
+            })
+                .then(response => {
+                    this.repos = response.data.data
+                    this.loading = false
+                })
+                .catch(error => {
+                    console.error(error)
+                    alert("通信中にエラーが発生しました。\nしばらく待ってからもう一度お試しください。\n\n" + error.message + (error.response ? "\n" + error.response.data.data : ""))
+                })
+        },
+        getIssues() {
+            history.replaceState('', '', '?repo=' + this.repo);
+
+            this.loading = true
+            axios.get(`api.php`, {
+                params: {
+                    repo: this.repo,
+                    action: "get-issues"
+                }
+            })
+                .then(response => {
+                    this.items = response.data.data
+                    this.loading = false
+                })
+                .catch(error => {
+                    console.error(error)
+                    console.log(error.response)
+                    alert("通信中にエラーが発生しました。\nしばらく待ってからもう一度お試しください。\n\n" + error.message + (error.response ? "\n" + error.response.data.data : ""))
+                })
+        },
+        upvote(item) {
+            if (item.upvote.voted) {
+                this.resetvote(item)
+                return
+            }
+            this.items = this.items.map(current => current.number === item.number ? {
+                ...current,
+                upvote: {
+                    ...current.upvote,
+                    loading: true,
+                    voted: !current.upvote.voted
+                },
+                downvote: {
+                    ...current.downvote,
+                    count: current.downvote.voted ? current.downvote.count - 1 : current.downvote.count,
+                    voted: false
+                },
+            } : current)
+
+            const params = new URLSearchParams()
+            params.append("number", item.number)
+            axios.post(`api.php?repo=${this.repo}&action=up`, params)
+                .then(() => {
+                    this.items = this.items.map(current => current.number === item.number ? {
+                        ...current,
+                        upvote: {
+                            ...current.upvote,
+                            count: current.upvote.count + 1,
+                            loading: false,
+                        },
+                    } : current)
+                })
+                .catch(error => {
+                    console.error(error)
+                    alert("通信中にエラーが発生しました。\nしばらく待ってからもう一度お試しください。\n\n" + error.message + (error.response ? "\n" + error.response.data.data : ""))
+                })
+        },
+        downvote(item) {
+            if (item.downvote.voted) {
+                this.resetvote(item)
+                return
+            }
+            this.items = this.items.map(current => current.number === item.number ? {
+                ...current,
+                upvote: {
+                    ...current.upvote,
+                    count: current.upvote.voted ? current.upvote.count - 1 : current.upvote.count,
+                    voted: false
+                },
+                downvote: {
+                    ...current.downvote,
+                    loading: true,
+                    voted: !current.downvote.voted
+                },
+            } : current)
+
+            const params = new URLSearchParams()
+            params.append("number", item.number)
+            axios.post(`api.php?repo=${this.repo}&action=down`, params)
+                .then(() => {
+                    this.items = this.items.map(current => current.number === item.number ? {
+                        ...current,
+                        downvote: {
+                            ...current.downvote,
+                            count: current.downvote.count + 1,
+                            loading: false,
+                        },
+                    } : current)
+                })
+                .catch(error => {
+                    console.error(error)
+                    alert("通信中にエラーが発生しました。\nしばらく待ってからもう一度お試しください。\n\n" + error.message + (error.response ? "\n" + error.response.data.data : ""))
+                })
+        },
+        resetvote(item) {
+            const votedType = item.upvote.voted ? "up" : "down"
+            this.items = this.items.map(current => current.number === item.number ? {
+                ...current,
+                upvote: {
+                    ...current.upvote,
+                    loading: votedType === "up" ? true : false,
+                },
+                downvote: {
+                    ...current.downvote,
+                    loading: votedType === "down" ? true : false,
+                },
+            } : current)
+
+            const params = new URLSearchParams()
+            params.append("number", item.number)
+            axios.post(`api.php?repo=${this.repo}&action=reset`, params)
+                .then(() => {
+                    this.items = this.items.map(current => current.number === item.number ? {
+                        ...current,
+                        upvote: {
+                            ...current.upvote,
+                            count: votedType === "up" ? current.upvote.count - 1 : current.upvote.count,
+                            voted: false,
+                            loading: false,
+                        },
+                        downvote: {
+                            ...current.downvote,
+                            count: votedType === "down" ? current.downvote.count - 1 : current.downvote.count,
+                            voted: false,
+                            loading: false,
+                        },
+                    } : current)
+                })
+                .catch(error => {
+                    console.error(error)
+                    alert("通信中にエラーが発生しました。\nしばらく待ってからもう一度お試しください。\n\n" + error.message + (error.response ? "\n" + error.response.data.data : ""))
+                })
+        }
+    },
+    watch: {
+        repo: function () {
+            this.getIssues()
+        }
+    }
+})
